3236a84b40a355234bb47a463a91cd5de4d5cc6d
nagelbagel <nagelbagel@gmail.com>
2010-01-25 23:48:56 +0000

    Added commented-out tests.

--- src/clientNativePackets.cpp
+++ src/clientNativePackets.cpp
@@ -1,16 +1,19 @@
 #include "nativePackets.h"
+#include <iostream>
 
 void ConnectionPacket::respondClient() const
 {
-	
+	//std::cout << "connected" << std::endl;
 }
 
 void DisconnectionPacket::respondClient() const
 {
+	//std::cout << "disconnected" << std::endl;
 	
 }
 
 void TamperPacket::respondClient() const
 {
 	
+	//std::cout << "tampered" << std::endl;
 }
--- src/serverNativePackets.cpp
+++ src/serverNativePackets.cpp
@@ -1,16 +1,20 @@
 #include "nativePackets.h"
+#include "user.h"
+#include <iostream>
 
 void ConnectionPacket::respondServer() const
 {
-	
+	//std::cout << "connected " << sender().username() << std::endl;
 }
 
 void DisconnectionPacket::respondServer() const
 {
+	//std::cout << "disconnected " << sender().username() << std::endl;
 	
 }
 
 void TamperPacket::respondServer() const
 {
+	//std::cout << "tamper " << sender().username() << std::endl;
 	
 }
