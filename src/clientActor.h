fb771d6d326b546e9a17c4e904678b8f72663bf7
nagelbagel <nagelbagel@gmail.com>
2010-01-31 23:15:53 +0000

    Added client packet receipt. No packets sent yet.

--- src/clientActor.h
+++ src/clientActor.h
@@ -4,21 +4,27 @@
 #include "actor.h"
 #include "clientAnimations.h"
 #include "animationManager.h"
+#include "hermite.h"
 
 class ClientActor : public Actor {
 public:
 
-    ClientActor(ActorID actorID, const Rect& rect, ClientAnimations::Animation animation);
+    ClientActor(ActorID actorID, const Rect& rect, const Vector2D& velocity, ClientAnimations::Animation animation);
     virtual ~ClientActor();
 
     virtual void OnCollision(Physical& other) { }
     AnimationManager::weak_ptr GetAnimation() const;
 
+    virtual void Update();
+	virtual void interpolate(double packetTime, const Vector2D& packetPosition, const Vector2D& packetVelocity);
+
 private:
 
     int                        _tempID;
     AnimationManager::weak_ptr _animation;
-
+	Vector2D                   _velocity;
+	HermiteInterpolation	   _hermite;
+	bool                       _useHermite;
 };
 
 #endif
--- src/clientCollisionPackets.cpp
+++ src/clientCollisionPackets.cpp
@@ -1,11 +1,37 @@
 #include "collisionPackets.h"
+#include "clientActor.h"
+#include "clientWorldInstance.h"
 #include <iostream>
 
 void CreationPacket::respondClient() const
 {
+	BOOST_FOREACH(const CreationUpdate& create, creation()) {
+		WorldInstance::ActorMap::const_iterator found =
+			ClientWorldInstance::current().GetActorMap().find(create.id);
+		if (found != ClientWorldInstance::current().GetActorMap().end()) {
+			delete found->second;
+		}
+
+		ClientActor *actor = new ClientActor(create.id, create.rect, create.velocity,
+			ClientAnimations::Animation(create.animation));
+	}
+	BOOST_FOREACH(const DestructionUpdate& destruct, destruction()) {
+		WorldInstance::ActorMap::const_iterator found =
+			ClientWorldInstance::current().GetActorMap().find(destruct.id);
+		if (found != ClientWorldInstance::current().GetActorMap().end()) {
+			delete found->second;
+		}
+	}
 }
 
 void MovementPacket::respondClient() const
 {
+	BOOST_FOREACH(const MovementUpdate& move, movement()) {
+		WorldInstance::ActorMap::const_iterator found =
+			ClientWorldInstance::current().GetActorMap().find(move.id);
+		if (found != ClientWorldInstance::current().GetActorMap().end()) {
+			found->second->interpolate(timestamp(), move.position, move.velocity);
+		}
+	}
 }
 
