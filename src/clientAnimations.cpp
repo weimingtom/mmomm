bc824d69ccf3292a45ecf71ab66c2713fc706e17
grandfunkdynasty <grandfunkdynasty@191a6260-07ae-11df-8636-19de83dc8dca>
2010-01-31 02:46:58 +0000

    Preprocessor magic! for animations

--- src/clientAnimations.cpp
+++ src/clientAnimations.cpp
@@ -3,12 +3,14 @@
 
 boost::unordered_map< ClientAnimations::Animation, int > ClientAnimations::_templates;
 
+#define ANIMATION( name, image, width, height, time ) img = ImageManager::current().getImage(image);\
+                                                      _templates[ name ] = AnimationManager::current().createAnimation(img, width, height, time);
 void ClientAnimations::Setup()
 {
-    // Create standard animations
-    ImageManager::shared_ptr img = ImageManager::current().getImage("001-Fighter01.png");
-    _templates[ FIGHTER ] = AnimationManager::current().createAnimation(img, 32, 48, 250);
+    ImageManager::shared_ptr img;
+    ANIMATION_LIST
 }
+#undef ANIMATION
 
 int ClientAnimations::Get(Animation animation)
 {
