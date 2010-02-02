8a1de19d5a1b686e6d9e23f74532aa046f8edb00
grandfunkdynasty <grandfunkdynasty@191a6260-07ae-11df-8636-19de83dc8dca>
2010-02-02 00:26:09 +0000

    Server sends updates to each user every ~0.1 seconds

--- src/serverWorldInstance.cpp
+++ src/serverWorldInstance.cpp
@@ -1,8 +1,11 @@
 #include "serverWorldInstance.h"
 #include "user.h"
+#include "frameTimer.h"
+#include "playerActor.h"
 #include <iostream>
 
 ServerWorldInstance::ServerWorldInstance()
+: _updatesOffset(0)
 {
 }
 
@@ -13,11 +16,18 @@ ServerWorldInstance::~ServerWorldInstance()
 void ServerWorldInstance::AddUser(User& user, PlayerActor* actor)
 {
     _userMap[&user] = actor;
+    _userList.push_front(&user);
 }
 
 void ServerWorldInstance::RemoveUser(User& user)
 {
     _userMap.erase(&user);
+    for ( UserList::iterator i = _userList.begin(); i != _userList.end(); i++ ) {
+        if ( *i == &user ) {
+            _userList.erase(i);
+            break;
+        }
+    }
 }
 
 PlayerActor* ServerWorldInstance::GetUserActor(User& user) const
@@ -37,4 +47,18 @@ ServerWorldInstance& ServerWorldInstance::current()
 {
     assert(dynamic_cast< ServerWorldInstance* >(&WorldInstance::current()));
     return *(ServerWorldInstance*)&WorldInstance::current();
+}
+
+void ServerWorldInstance::Update(bool elapsed)
+{
+    WorldInstance::Update(elapsed);
+    _updatesOffset += _userList.size() * 10.0 / FrameTimer::current().framerate();
+    int updates = int(_updatesOffset);
+    _updatesOffset -= updates;
+    for ( int i = 0; i < updates; i++ ) {
+        User* user = _userList.front();
+        _userList.pop_front();
+        user->sendNetworkUpdate(_userMap[user]);
+        _userList.push_back(user);
+    }
 }
