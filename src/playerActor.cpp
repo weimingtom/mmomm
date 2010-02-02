75ae13d0948cdbe5b44c38da46d484de92562ff1
nagelbagel <nagelbagel@gmail.com>
2010-02-02 00:00:47 +0000

    Movement packet receiving on server.

--- src/playerActor.cpp
+++ src/playerActor.cpp
@@ -1,9 +1,11 @@
 #include "playerActor.h"
 #include "user.h"
+#include "frameTimer.h"
 
 PlayerActor::PlayerActor(User& user, const Rect& rect)
 : Actor(rect)
 , _user(user)
+, _updateTime(FrameTimer::current().frameTime())
 {
 }
 
--- src/serverCollisionPackets.cpp
+++ src/serverCollisionPackets.cpp
@@ -1,10 +1,31 @@
 #include "collisionPackets.h"
-#include "user.h"
+#include "frameTimer.h"
 #include "networkServer.h"
+#include "playerActor.h"
+#include "serverWorldInstance.h"
+#include "user.h"
 #include <iostream>
 
 void MovementPacket::respondServer() const
 {
-	
+	PlayerActor *actor = ServerWorldInstance::current().GetUserActor(sender());
+	if (!actor) {
+		std::cout << "Received a movement packet for ";
+		std::cout << sender().username() << " when no actor is assigned." << std::endl;
+	}
+	BOOST_FOREACH(const MovementUpdate& update, movement()) {
+		if (update.id != actor->id()) {
+			std::cout << "Received a movement update for ";
+			std::cout << sender().username() << " for non-player actor." << std::endl;
+			continue;
+		}
+		if (actor->getUpdateTime() < timestamp()) {
+			actor->setUpdateTime(timestamp());
+			double time = FrameTimer::current().frameTime() - timestamp();
+			Vector2D position = update.position + update.velocity * time;
+			actor->SetPosition(position);
+			actor->SetVelocity(update.velocity);
+		}
+	}
 }
 
