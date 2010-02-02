b5df644bf5eac44e5c1b8cc22a6da6d7898bc83d
nagelbagel <nagelbagel@gmail.com>
2010-02-02 00:47:01 +0000

    Cleaned up serverNativePackets.

--- src/serverNativePackets.cpp
+++ src/serverNativePackets.cpp
@@ -14,36 +14,19 @@ void ConnectionPacket::respondServer() const
     const ServerWorldInstance::UserMap& users = ServerWorldInstance::current().GetUserMap();
     PlayerActor* actor = new PlayerActor(sender(), Rect(1.0 * users.size(), 0.0, 1.0 * (1 + users.size()), 1.0));
 
-    // Tell existing users about the new actor
+    // Tell the user about the new actor; others will get it later
     CreationUpdate update;
     update.id = actor->id();
     update.rect = actor->GetCollisionRect();
-    update.velocity = Vector2D(0, 0);
-    update.sprite = ClientSprites::FIGHTER;
-    update.isClientPlayer = false;
-
+    update.velocity = actor->GetVelocity();
+    update.sprite = actor->GetSpriteType();
+    update.isClientPlayer = true;
+	
     CreationPacket::CreationList creationList;
     creationList.push_back(update);
     CreationPacket::DestructionList destructionList;
-
-    for ( ServerWorldInstance::UserMap::const_iterator i = users.begin(); i != users.end(); i++ ) {
-        CreationPacket packet(creationList.begin(), creationList.end(), destructionList.begin(), destructionList.end());
-        NetworkServer::current().send(packet, *(i->first));
-    }
-
-    // Tell the new user about existing actors
-    const WorldInstance::ActorMap& actors = WorldInstance::current().GetActorMap();
-    CreationPacket::CreationList existingList;
-    for ( WorldInstance::ActorMap::const_iterator i = actors.begin(); i != actors.end(); i++ ) {
-        CreationUpdate update;
-        update.id = i->first;
-        update.rect = i->second->GetCollisionRect();
-        update.velocity = Vector2D(0, 0);
-        update.sprite = ClientSprites::FIGHTER;
-        update.isClientPlayer = i->second == actor;
-        existingList.push_back(update);
-    }
-    CreationPacket packet(existingList.begin(), existingList.end(), destructionList.begin(), destructionList.end());
+	
+    CreationPacket packet(creationList.begin(), creationList.end(), destructionList.begin(), destructionList.end());
     NetworkServer::current().send(packet, sender());
 
     ServerWorldInstance::current().AddUser(sender(), actor);
@@ -53,21 +36,8 @@ void DisconnectionPacket::respondServer() const
 {
 	std::cout << "disconnected " << sender().username() << std::endl;
 
-    DestructionUpdate update;
-    update.id = ServerWorldInstance::current().GetUserActor(sender())->id();
-
     delete ServerWorldInstance::current().GetUserActor(sender());
     ServerWorldInstance::current().RemoveUser(sender());
-
-    CreationPacket::CreationList creationList;
-    CreationPacket::DestructionList destructionList;
-    destructionList.push_back(update);
-
-    const ServerWorldInstance::UserMap& users = ServerWorldInstance::current().GetUserMap();
-    for ( ServerWorldInstance::UserMap::const_iterator i = users.begin(); i != users.end(); i++ ) {
-        CreationPacket packet(creationList.begin(), creationList.end(), destructionList.begin(), destructionList.end());
-        NetworkServer::current().send(packet, *(i->first));
-    }
 }
 
 void TamperPacket::respondServer() const
